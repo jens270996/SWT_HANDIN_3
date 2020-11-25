0b0a6932cb4d7f6671495999f2b7c7bbf5a879ea
Jens Kristensen <jens-nk@hotmail.com>
2020-11-25 13:10:08 +0100

    fsa

--- Microwave.Test.Integration/1_Door_UI.cs
+++ Microwave.Test.Integration/1_Door_UI.cs
@@ -2,11 +2,13 @@ using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
-    public class Tests
+    public class Door_UI
     {
         [SetUp]
         public void Setup()
         {
+
+            UserInterface ui = new UserInterface(powerButton, timeButton, startCancelButton, door, display, light, cooker);
         }
 
         [Test]
