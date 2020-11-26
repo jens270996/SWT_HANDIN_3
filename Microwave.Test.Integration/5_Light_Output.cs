8c7abb81887e3bdd97e1696778c75db7d7ca6e76
jens270996 <55832697+jens270996@users.noreply.github.com>
2020-11-26 12:56:48 +0100

    step 5

---
+++ Microwave.Test.Integration/5_Light_Output.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Interfaces;
+using Microwave.Classes.Controllers;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    public class _5_Light_Output
+    {
+        private IOutput output;
+        private ILight light;
+        
+        private StringWriter str;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            output = new Output();
+            light = new Light(output);
+            
+            str = new StringWriter();
+            Console.SetOut(str);
+        }
+
+
+        [Test]
+        public void LightOn_LoggedLightOn()
+        {
+            light.TurnOn();
+            StringAssert.Contains("Light is turned on", str.ToString());
+            
+        }
+
+        [Test]
+        public void LightOff_LoggedLightOff()
+        {
+            light.TurnOn();
+            light.TurnOff();
+            StringAssert.Contains("Light is turned off", str.ToString());
+
+        }
+    }
+}
