c9ec93b7fd2fcb88aece835eb22da86c13e16441
Joachim <Kroyer.joachim@gmail.com>
2020-11-26 16:02:17 +0100

    start of step 9 added

---
+++ Microwave.Test.Integration/9_CookController_timer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Interfaces;
+using Microwave.Classes.Controllers;
+using NSubstitute;
+using NUnit.Framework;
+
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class CookController_timer
+    {
+        private ITimer timer;
+        private ICookController cookController;
+        private IPowerTube powerTube;
+        private IOutput output;
+        private IDisplay display;
+        private IUserInterface userInterface;
+        
+        [SetUp]
+        public void Setup()
+        {
+            output = new Output();
+            display = new Display(output);
+            timer = new Timer();
+            powerTube = new PowerTube(output);
+            userInterface = Substitute.For<IUserInterface>();
+            cookController = new CookController(timer, display, powerTube, userInterface);
+        }
+
+        [Test]
+        public void 
+
+    }
+}
