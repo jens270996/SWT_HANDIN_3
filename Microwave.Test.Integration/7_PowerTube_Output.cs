93ed6cb04459ddab8e6095adcf18a3f2581eacd7
Joachim <Kroyer.joachim@gmail.com>
2020-11-26 12:42:41 +0100

    Test for step 7 added(not finished)

---
+++ Microwave.Test.Integration/7_PowerTube_Output.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Interfaces;
+using Microwave.Classes.Controllers;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class PowerTube_Output
+    {
+   
+        private IOutput output;
+        private IPowerTube powerTube;
+        private CookControllerDriver cookControllerDriver;
+        private StringWriter str;
+        
+
+        [SetUp]
+        public void Setup()
+        {
+            output = new Output();
+            powerTube = new PowerTube(output);
+            cookControllerDriver = new CookControllerDriver(powerTube);
+        }
+
+        [TestCase(1)]
+        [TestCase(37)]
+        [TestCase(100)]
+        public void StartCooking_TurnOnCalled(int value)
+        {
+            str = new StringWriter();
+            Console.SetOut(str);
+            cookControllerDriver.StartCooking(value);
+            StringAssert.Contains( $"PowerTube works with {value}", str.ToString());
+        }
+    }
+}
---
+++ Microwave.Test.Integration/CookControllerDriver.cs
@@ -0,0 +1,24 @@
+﻿using Microwave.Classes.Interfaces;
+
+namespace Microwave.Test.Integration
+{
+    public class CookControllerDriver 
+    {
+        private IPowerTube myPowerTube;
+
+        public CookControllerDriver(IPowerTube powerTube)
+        {
+            myPowerTube = powerTube;
+        }
+
+        public void StartCooking (int power)
+        {
+            myPowerTube.TurnOn(power);
+        }
+
+        public void Stop()
+        {
+            myPowerTube.TurnOff();
+        }
+    }
+}
